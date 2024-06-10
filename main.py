43add21bc4b0a1f3c1faf9ab75ffd07612c8f3fb
Daniel Mitchell <49624246+DanielMitchell444@users.noreply.github.com>
2024-06-10 07:46:32 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,319 @@
+'''
+Please follow the directions accordingly and answer the questions to the best
+of your ability. You will have 50 minutes to complete this exam, if you
+need more time, then more time will be given.
+'''
+
+'''
+Points Possible: /2
+QUESTION 1:
+Create 3 different variables, one should be a string, one should be
+an integer, and one should be a boolean.
+Please post the code below this comment
+'''
+
+'''
+Points Possible: /2
+QUESTION 2:
+In your own words, give a short description of each data type below
+String:
+Boolean:
+Integer:
+'''
+
+'''
+Points Possible: /2
+QUESTION 3:
+Below you will see two variables. Using the variables below,  add the
+two numbers together and store the sum inside a sum variable,
+then, print out the sum variable.
+Please post the code below this comment
+'''
+
+numberOne = 1
+numberTwo = 2
+
+'''
+Points Possible: /2
+QUESTION 4:
+Using the two string variables below, please use to add the two strings together.
+Please post the code below this comment
+'''
+
+firstName = "Jeff"
+lastName = "Bezos"
+
+
+'''
+Points Possible: /2
+QUESTION 5:
+Please create a list below this comment with 3 items of your choosing
+'''
+
+'''
+Points Possible: /2
+QUESTION 6:
+Using the proper sytax for accesing indexes, please select the
+1st index of your list
+'''
+
+'''
+Points Possible: /2
+QUESTION 7:
+Using the proper syntax for adding elements to lists,
+please add fortnite to the end of the list
+'''
+
+'''
+Points Possible: /2
+QUESITON 8:
+Using the proper syntax for removing elements from a list,
+please remove the word fortnite from the list
+'''
+
+'''
+Points Possible: /2
+QUESTION 9:
+Using the proper syntax, please sort the list from
+acending to decending order
+'''
+
+'''
+Points Posisble: /2
+QUESTION 10:
+Using the proper syntax, please reverse the items
+inside the list.
+'''
+
+'''
+Points Possible: /2
+QUESTION 11:
+Below you will see a variable with the name of score.
+that has the value of 2
+Using the proper syntax, first check to see if
+score is less than 2, if it isn't
+print out wrong
+After that, make another if statement that checks to see
+if score is greater than 2, if it isn't, print out
+wrong again.
+Then, create an else statement that prints out 
+both are wrong
+Please post the code below this comment
+'''
+score = 2
+'''
+Points Possible: /2
+QUESTION 12:
+Below, you will see a list that is titled redbullFlavors.
+Using the proper syntax, please loop through the list
+and print out each item in the list.
+Please post the code below this comment
+'''
+list = ["Juneberry", "Blue Raspbery", "Apricot"]
+'''
+Points Possible: /2
+QUESTION 13:
+Below this comment, you will see a variable called inventory that is equal to
+0. 
+I want you to create a while loop that checks to see if inventory is less than 5,
+if it is, then increment the value until it is equal to 5.
+HINT: To increment a number, you will go +=1.
+So essentially, you want to go inventory+= 1
+'''
+inventory = 0
+'''
+Points Possible: /2
+QUESTION 14:
+Below this comment, i want you to create a function called add that passes
+2 parameters, numberOne, and numberTwo inside it. Then, i want you to
+return the sum of the two numbers.
+Please post the code below this comment.
+'''
+
+'''
+Points Possible: /2
+QUESTION 15:
+Below this comment, i want you to call the function and pass
+two numbers inside the function arguement.
+'''
+
+'''
+Points Possible: /2
+QUESTION 16:
+Below, you will see a string with the variable name Mario.
+Using a for loop, please loop through the string
+and print out the string.
+Please post the code below this comment
+'''
+
+mario = "Mario"
+
+'''
+Points Possible: /2
+QUESTION 17:
+Using the variable Mario above, please use the
+.upper() method to make the variable upperCase.
+'''
+
+'''
+Points Possible: /2
+QUESTION 18:
+Using the variable mario above, please use the
+.lower() method to make the variable lowercase
+'''
+
+'''
+Points Possible: /2
+QUESTION 19:
+Using the variable mario above, get the first 2 indexes of the string
+Create a variable named x and then store the indexed strings inside
+the variable
+'''
+'''
+Points Possible: /2
+QUESTION 20:
+Below this comment, you will see two lists. One titled adj and one
+titled fruits. I want you to use a nested for loop to loop through both
+of the lists, then, i want you to print the two values.
+
+'''
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+
+
+'''
+Points Possible: /2
+QUESTION 21:
+Create a dictionary called car that takes a brand, a model, and a year
+as a key. Post the code below this comment
+'''
+person2 = {
+  "name": "Daniel",
+   "Age": "26",
+   "Gender": "Male"
+}
+''''
+Points Possible: /2
+QUESTION 22:
+Below is a dictionary named person2 that gives information
+about the person.
+'''
+
+person2 = {
+  "name": "Marco",
+  "Age": "12",
+  "gender": "male",
+
+}
+
+'''
+Below this comment, access the age key using the appropriate syntax.
+'''
+
+
+'''
+Points Possible: /2
+QUESTION 23:
+Using the dictionary above, please change the age of the person2
+dictionary from 12 to 18 using the appropriate syntax. Please post 
+the code below this comment.
+'''
+
+
+'''
+Points Possible: /2
+QUESTION 24:
+Using the dictionary above, please add a country key with the value of
+United States. Please post the code below this comment
+'''
+
+'''
+Points Possible: /2
+QUESTION 25:
+Using the dictionary person2, please remove the gender key name
+using the appropriate syntax. Please post the code below
+this comment.
+'''
+
+
+'''
+Points Possible: /2
+QUESTION 26:
+Using the dictionary person2, loop through the dictionary and print
+out all the key names. Post the code below this comment
+'''
+
+'''
+Points Possible: /2
+QUESTION 27:
+Below this comment, create a nested dictionary.
+'''
+
+
+'''
+Points Possible: /2
+QUESTION 28:
+Finish this song lyric.
+I play pokemon go ____
+TYPE ANSWER HERE:
+'''
+
+'''
+Points Possible: /2
+QUESTION 29:
+Will the below code work? Yes or no?>
+TYPE ANSWER HERE:
+'''
+
+person1 = {
+ "name": "Daniel",
+ "Age": "26",
+ "Gender": "Male",
+ "Gender": "Male"
+}
+
+'''
+Points Possible: /2
+QUESTION 30:
+Below this comment, create a tuple of 3 values
+HINT: ()
+'''
+
+'''
+Points Possible: /2
+QUESTION 31:
+Below this comment, print out and access the 2nd tuple index
+'''
+
+'''
+Points Possible: /2
+QUESTION 32:
+What is the color your shoes right now?
+TYPE ANSWER HERE:
+'''
+'''
+Points Possible: /2
+QUESTION 33:
+Below is a tuple, below this comment in the answer part provided,
+please explain if this is valid code
+TYPE ANSWER HERE:
+'''
+pokemon = ("squirtle", "Chimchar", "Pipilup")
+
+pokemon[1] = "Squirtle"
+
+
+'''
+Points Possible: /2
+QUESTION 34:
+Below is another tuple, below this comment, please specify
+if the code is valid
+TYPE ANSWER HERE:
+'''
+
+pokemon2 = ("Charmander", "Warturtle", "Pikachu", "Pikachu")
+
+'''
+FINAL GRADE: /68
+'''
